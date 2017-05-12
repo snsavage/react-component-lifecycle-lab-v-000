ea044cc84573359a74cef8f86a2e88e4363c9101
Scott Savage <snsavage@gmail.com>
2017-05-12 14:34:09 -0400

    Pass all lab specs

--- components/TweetWall.js
+++ components/TweetWall.js
@@ -10,10 +10,19 @@ export default class TweetWall extends React.Component {
   }
 
   // TODO: componentWillMount()
+  componentWillMount() {
+    this.state.tweets = this.props.newTweets;
+  }
 
   // TODO: shouldComponentUpdate()
+  shouldComponentUpdate(nextProps) {
+    return (nextProps.newTweets.length > 0)
+  }
 
   // TODO: componentWillReceiveProps()
+  componentWillReceiveProps(nextProps) {
+    this.state.tweets = [...nextProps.newTweets, ...this.state.tweets];
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => {
